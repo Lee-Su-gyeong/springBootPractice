2369b42dd08b706f112bc282f519881303fcd8a9
10-N3121\user <dune2007@idstrust.com>
2023-02-07 16:49:41 +0900

    20230207 HttpServletRequest의 개요

---
+++ src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -0,0 +1,9 @@
+package hello.servlet.basic.request;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(name = "requestHeaderServlet" )
+public class RequestHeaderServlet extends HttpServlet {
+
+}
